673495364cd5394256960aa6f56d9a5aae2f1e10
Andy <andy@dragonchaos.com>
2012-06-07 17:20:13 -0400

    changed author

---
+++ ColdFusion/readme.md
@@ -0,0 +1,139 @@
+# ColdFusion Package for Sublime Text 2
+
+### What's New
+* Added Command ```ColdFusion: Insert UDF``` (6/2/12)
+
+### Features
+
+* ColdFusion cfml, and script-based CFCs syntax highlighting. (see tmLanguage files)
+* Auto inserts closing hash # (see Default (platform).sublime-keymap)
+* Provides completions that match just after typing an opening angle bracket (see cfml_completions.py)
+* Exhaustive library of snippet bindings (see ColdFusion.sublime-completions)
+* ColdFusion specific key bindings (see Default (platform).sublime-keymap)
+
+## Installation
+
+The recommended method of installation is via Package Control.
+
+### Package Control
+
+* Follow instructions on http://wbond.net/sublime_packages/package_control
+* Install using Package Control: Install > ColdFusion package
+
+### Using Git
+
+Go to your Sublime Text 2 `Packages` directory and clone the repository using the command below:
+
+    git clone https://github.com/SublimeText/ColdFusion
+
+### Download Manually
+
+* Download the files using the GitHub .zip download option
+* Unzip the files and rename the folder to `ColdFusion`
+* Copy the folder to your Sublime Text 2 `Packages` directory
+
+- - -
+
+## Key bindings
+
+<table>
+    <tr>
+        <td>
+<kbd>shift</kbd>+<kbd>3</kbd> <code>#SELECTION#</code>
+        </td>
+        <td>
+<kbd>ctrl</kbd>+<kbd>shift</kbd>+<kbd>o</kbd> <code>&lt;cfoutput&gt;SELECTION&lt;/cfoutput&gt;</code>
+        </td>
+    </tr>
+    <tr>
+        <td>
+<kbd>ctrl</kbd>+<kbd>shift</kbd>+<kbd>3</kbd> <code>&lt;cfoutput&gt;#SELECTION#&lt;/cfoutput&gt;</code>
+        </td>
+        <td>
+<kbd>ctrl</kbd>+<kbd>shift</kbd>+<kbd>a</kbd> <code>&lt;cfabort /&gt;</code>
+        </td>
+    </tr>
+    <tr>
+        <td>
+<kbd>ctrl</kbd>+<kbd>shift</kbd>+<kbd>r</kbd> <code>&lt;cfscript&gt;SELECTION&lt;/cfscript&gt;</code>
+        </td>
+        <td>
+<kbd>ctrl</kbd>+<kbd>shift</kbd>+<kbd>d</kbd> <code>&lt;cfdump var=&quot;#SELECTION#&quot;&gt;</code>
+        </td>
+    </tr>
+    <tr>
+        <td>
+<kbd>ctrl</kbd>+<kbd>shift</kbd>+<kbd>c</kbd> <code>cfml comment</code>
+        </td>
+        <td>
+<kbd>ctrl</kbd>+<kbd>alt</kbd>+<kbd>shift</kbd>+<kbd>/</kbd> <code>javadoc comment</code>
+        </td>
+    </tr>
+    <tr>
+        <td>
+<kbd>alt</kbd>+<kbd>enter</kbd> <code>terminates statement; adds new line</code>
+        </td>
+        <td>
+<kbd>ctrl</kbd>+<kbd>shift</kbd>+<kbd>=</kbd> <code>&lt;cfset SELECTION /&gt;</code>
+        </td>
+    </tr>
+</table>
+
+###### Overridden Sublime Key bindings
+
+<table>
+    <tr>
+        <td>
+<kbd>ctrl</kbd>+<kbd>alt</kbd>+<kbd>d</kbd> <code>duplicate line</code>
+        </td>
+        <td>
+<kbd>ctrl</kbd>+<kbd>alt</kbd>+<kbd>a</kbd> <code>expand selection to tag</code>
+        </td>
+    </tr>
+</table>
+
+##Completions and Snippets
+**Note**: A classic completions plug-in is available from @bbluemel which can be downloaded at
+
+https://github.com/bbluemel/ColdFusionTagCompletions
+***
+Although, this package includes CFScript completions (```.sublime-completions```) and ColdFusion tag completions (API-injected), it's recommended that you create your own custom snippets using code that fits your particular coding style.
+To create a new snippet select ```Tools > New Snippet...``` from the Sublime Text menu.
+Use ```<scope>text.html.cfm - string</scope>``` for ColdFusion markup snippets and ```<scope>source.cfscript - string</scope>``` for CFScript snippets.
+
+Completions in order of priority are:
+* Snippets
+* API-injected completions
+* ```.sublime-completions``` files
+* words in the current buffer
+
+The following list contains snippets repositories made available by ColdFusion community members:
+
+* https://github.com/indynagpal/sublime-stuff
+* https://github.com/bittersweetryan/ColdFusion-Script-Sublime-Snippets
+
+To install snippets just put the ```.sublime-snippet``` files in your Packages/User folder or sub-folder (to get to your Packages/User folder select ```Preferences > Browse Packages...``` from the Sublime Text 2 menu).
+
+##Additional Packages
+Some additional packages you might want to check out (these packages are all available through Package Control):
+
+* https://github.com/BoundInCode/AutoFileName
+* https://github.com/weslly/ColorPicker
+* https://github.com/xobb1t/Sublime-AdvancedNewFile
+* https://github.com/kemayo/sublime-text-2-git
+* https://github.com/dz0ny/LiveReload-sublimetext2
+* https://github.com/bgreenlee/sublime-github
+* https://github.com/virtix/sublime-text-2-mxunit
+
+These packages require manual installation
+
+* https://github.com/DominicWatson/SublimeText2CfQuickDocsLauncher
+
+##License
+Copyright (c) 2012
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ Local History/history.py
@@ -0,0 +1,235 @@
+import sublime
+import sublime_plugin
+import os
+import platform
+from collections import defaultdict
+import cPickle as pickle
+from datetime import datetime as dt
+import difflib
+import filecmp
+import shutil
+from threading import Thread
+
+#------------#
+#   Config   #
+#------------#
+HISTORY_LIMIT = 50
+FILE_SIZE_LIMIT = 262144  # 256 KB
+
+# Paths
+history_path = os.path.join(os.path.expanduser("~"), ".sublime", "history")
+map_path = os.path.join(history_path, ".map")
+
+
+def create_history_dir_map():
+    if not os.path.exists(history_path):
+        os.makedirs(history_path)
+        pickle.dump(defaultdict(list), open(map_path, "wb"), -1)
+create_history_dir_map()
+
+
+def show_diff(window, diff):
+    panel = window.new_file()
+    panel.set_scratch(True)
+    panel.set_syntax_file("Packages/Diff/Diff.tmLanguage")
+    panel_edit = panel.begin_edit()
+    panel.insert(panel_edit, 0, diff)
+    panel.end_edit(panel_edit)
+
+
+def get_filedir(file_path):
+    file_dir = os.path.dirname(file_path)
+    if platform.system() == "Windows":
+        if file_dir.find("\\") == 0:
+            file_dir = file_dir[2:]  # Strip the network \\ starting path
+        if file_dir.find(":") == 1:
+            file_dir = file_dir.replace(":", "", 1)
+    else:
+        file_dir = file_dir[file_dir.find(os.sep) + 1:]  # Trim the root
+    return os.path.join(history_path, file_dir)
+
+
+class HistorySave(sublime_plugin.EventListener):
+
+    def on_post_save(self, view):
+
+        def run(file_path):
+            # Return if file exceeds the limit
+            if os.path.getsize(file_path) > FILE_SIZE_LIMIT:
+                print "WARNING: Local History did not save a copy of this file because it has exceeded {0}KB.".format(FILE_SIZE_LIMIT / 1024)
+                return
+
+            file_name = os.path.basename(file_path)
+            newfile_name = "{0}.{1}".format(dt.now().strftime("%b.%d.%Y.%H.%M.%S"), file_name)
+            newfile_dir = get_filedir(file_path)
+            newfile_path = os.path.join(newfile_dir, newfile_name)
+
+            # Load history map
+            with open(map_path, "rb") as map:
+                history_map = pickle.load(map)
+                history_list = history_map[file_path]
+
+            # Skip if no changes
+            if history_list:
+                if filecmp.cmp(file_path, os.path.join(newfile_dir, history_list[0])):
+                    return
+
+            # Store history
+            if not os.path.exists(newfile_dir):
+                os.makedirs(newfile_dir)  # Create directory structure
+            shutil.copyfile(file_path, newfile_path)
+
+            with open(map_path, "wb") as map:
+                # Add reference to map
+                history_list.insert(0, newfile_name)
+
+                # Remove old files
+                for file in history_list[HISTORY_LIMIT + 1:]:
+                    os.remove(os.path.join(newfile_dir, file))
+                # Remove reference from map
+                del history_list[HISTORY_LIMIT + 1:]
+
+                # Dump history map
+                pickle.dump(history_map, map, -1)
+
+        # Process in a thread
+        t = Thread(target=run, args=(view.file_name(),))
+        t.start()  # test
+
+
+class HistoryOpen(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+        # Fetch history
+        with open(map_path, "rb") as map:
+            history_map = pickle.load(map)
+            history_list = history_map[self.view.file_name()]
+            # Skip the first one as its always identical
+            files = history_list[1:]
+            if not files:
+                sublime.status_message("No Local History Found")
+                return
+
+        def on_done(index):
+            # Escape
+            if index == -1:
+                return
+
+            # Open
+            file_dir = get_filedir(self.view.file_name())
+            self.view.window().open_file(os.path.join(file_dir, files[index]))
+
+        self.view.window().show_quick_panel(files, on_done)
+
+
+class HistoryCompare(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+        # Fetch history
+        with open(map_path, "rb") as map:
+            history_map = pickle.load(map)
+            history_list = history_map[self.view.file_name()]
+            # Skip the first one as its always identical
+            files = history_list[1:]
+            if not files:
+                sublime.status_message("No Local History Found")
+                return
+
+        def on_done(index):
+            # Escape
+            if index == -1:
+                return
+
+            # Trigger save before comparing, if required!
+            if self.view.is_dirty():
+                self.view.run_command("save")
+
+            # From
+            from_file = files[index]
+            file_dir = get_filedir(self.view.file_name())
+            from_file_path = os.path.join(file_dir, from_file)
+            with open(from_file_path, "r") as f:
+                from_content = f.readlines()
+
+            # To
+            file_path = self.view.file_name()
+            to_file = os.path.basename(file_path)
+            with open(file_path, "r") as f:
+                to_content = f.readlines()
+
+            # Compare and show diff
+            diff = difflib.unified_diff(from_content, to_content, from_file, to_file)
+            show_diff(self.view.window(), "".join(diff))
+
+        self.view.window().show_quick_panel(files, on_done)
+
+
+class HistoryReplace(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+        # Fetch history
+        with open(map_path, "rb") as map:
+            history_map = pickle.load(map)
+            history_list = history_map[self.view.file_name()]
+            # Skip the first one as its always identical
+            files = history_list[1:]
+            if not files:
+                sublime.status_message("No Local History Found")
+                return
+
+        def on_done(index):
+            # Escape
+            if index == -1:
+                return
+
+            # Replace
+            file = files[index]
+            file_dir = get_filedir(self.view.file_name())
+            file_path = os.path.join(file_dir, file)
+            with open(file_path, "r") as f:
+                self.view.replace(edit, sublime.Region(0, self.view.size()), f.read())
+            self.view.run_command("save")
+
+        self.view.window().show_quick_panel(files, on_done)
+
+
+class HistoryIncrementalDiff(sublime_plugin.TextCommand):
+
+    def run(self, edit, **kwargs):
+        with open(map_path, "rb") as map:
+            history_map = pickle.load(map)
+            history_list = history_map[self.view.file_name()]
+            if len(history_list) < 2:
+                sublime.status_message("No Incremental Diff Found")
+                return
+            files = history_list[:-1]
+
+        def on_done(index):
+            # Escape
+            if index == -1:
+                return
+
+            if len(history_list) >= 1:
+                from_file = history_list[index + 1]
+                file_dir = get_filedir(self.view.file_name())
+                from_file_path = os.path.join(file_dir, from_file)
+                with open(from_file_path, "r") as f:
+                    from_content = f.readlines()
+
+                to_file = history_list[index]
+                file_dir = get_filedir(self.view.file_name())
+                to_file_path = os.path.join(file_dir, to_file)
+                with open(to_file_path, "r") as f:
+                    to_content = f.readlines()
+                diff = difflib.unified_diff(from_content, to_content, from_file, to_file)
+                show_diff(self.view.window(), "".join(diff))
+
+        self.view.window().show_quick_panel(files, on_done)
+
+
+class HistoryDeleteAll(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+        shutil.rmtree(history_path)
+        create_history_dir_map()
+        sublime.status_message("All Local History Deleted")
